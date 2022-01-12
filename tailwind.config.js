84eba3fa4a7853e830c0196d076b8e81e6253d18
ColeDrain <cole.drain@yahoo.com>
2022-01-12 11:56:42 +0100

    major mods

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nova: "'Nova Flat', cursive",
+      },
+    },
   },
   variants: {
     extend: {},
